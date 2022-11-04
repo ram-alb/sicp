c445552ba5ce0e3d46a98cbe3258d921bf266f11
Ramil Albakov <ramil.albakov2@gmail.com>
2022-11-04 15:55:29 +0600

    add test of sqrt function with small number

--- tests/chapter1/src/test_part_1_1_7.py
+++ tests/chapter1/src/test_part_1_1_7.py
@@ -1,6 +1,7 @@
 """Test functions from chapter1.src.part_1_1_7 module."""
 
 from math import sqrt as math_sqrt
+import pytest
 from sicp.chapter1.src.part_1_1_7 import sqrt
 
 
@@ -12,3 +13,9 @@ def test_sqrt():
     assert round(math_sqrt(21), 2) == round(sqrt(21), 2)
     assert round(math_sqrt(10000)) == round(sqrt(10000))
     assert round(math_sqrt(0.9), 2) == round(sqrt(0.9), 2)
+
+
+def test_sqrt_raises():
+    """Test if sqrt function does not work with tiny numbers."""
+    with pytest.raises(AssertionError):
+        assert round(math_sqrt(0.0001), 2) == round(sqrt(0.0001), 2)
